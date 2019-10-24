8bd7393ad3f32922964361f4f1142b996e3ba9e3
葛明明 <gemingming-js@sino-carbon.cn>
2019-10-24 11:27:05 +0800

    no message

--- Jenkins/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- # 停止原来服务并删除镜像
- echo "start"
- cd $1/install/helm
- helm template app --values app/$4.yaml > $1/$2.$3.yaml
- ln -sf $1/$2.$3.yaml  $1/$2.yaml
- #kubectl delete -f    $1/$2.yaml
- kubectl apply  -f    $1/$2.yaml
- echo "end"
--- Jenkins/deploy_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-  ssh -p 3906 root@221.226.116.222 "cd ${1}/install/helm && \
-    helm template app --values app/${4}.yaml > ${1}/${2}.${3}.yaml && \
-    ln -sf ${1}/${2}.${3}.yaml  ${1}/${2}.yaml && \
-    kubectl delete  -f    ${1}/${2}.yaml  && \
-    sleep 15 && \
-    kubectl apply  -f    ${1}/${2}.yaml" 
